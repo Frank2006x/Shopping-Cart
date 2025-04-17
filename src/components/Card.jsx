e6cbb567746e87dd0fab222feb1e34ad63b897b9
Frank2006x <frank2006x@gmail.com>
2025-04-17 12:31:51 +0530

    implement search bar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ element }) => {
   return (
     <div className="card w-60  rounded-2xl overflow-hidden shadow-lg bg-[#1c1a1ad1]
-     text-white transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_#55ff88ba] border border-zinc-800">
+     text-white transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_lightgrey] border border-zinc-800">
       <img
         loading="lazy"
         src={element.background_image}
@@ -14,7 +14,7 @@ const Card = ({ element }) => {
         <h1 className="text-xl font-semibold text-green-20 truncate">
           {element.name}
         </h1>
-        <p>{element.released.slice(0, 4)}</p>
+        <p>{element.released ? element.released.slice(0, 4) : null}</p>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Loader from "../UI/Loader";
 import InfiniteLoader from "../UI/InfiniteLoader";
 import { createPortal } from "react-dom";
+import Search from "../UI/Input";
+
 import { useRef } from "react";
 import { useAsyncList } from "@react-stately/data";
 import { useInView } from "react-intersection-observer";
 
 const Home = () => {
   const [onStart, setOnStart] = useState(true);
+  const [startWith, setStartWith] = useState("");
+  const [onSearch, setOnSearch] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -21,7 +25,7 @@ const Home = () => {
       // Otherwise, the cursor is the next URL to load, as returned from the previous page.
       let res = await fetch(
         cursor ||
-          `https://api.rawg.io/api/games?key=9089809c8f4c48c78233c2c5373a411b&page=1`,
+          `https://api.rawg.io/api/games?key=9089809c8f4c48c78233c2c5373a411b&page=1&search=${startWith}`,
         {
           signal,
         }
@@ -34,24 +38,45 @@ const Home = () => {
     },
   });
   const { ref, inView } = useInView({
-    // rootMargin: '1000px', not to show loading 
+    // rootMargin: '1000px', not to show loading
   });
-  const listRef=useRef(list);
-  useEffect(()=>{
-    listRef.current=list;
-  },[list])
+  
+  const listRef = useRef(list);
+  useEffect(() => {
+    try {
+      list.reload();
+      setOnSearch(true);
+      if (startWith===""){
+        setOnSearch(false)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [startWith]);
+  
+  useEffect(() => {
+    listRef.current = list;
+  }, [list]);
   useEffect(() => {
     if (listRef.current.items.length && inView && !list.isLoading) {
       listRef.current.loadMore();
-      
     }
   }, [inView]);
-
-
 
   return (
     <>
       {onStart && createPortal(<Loader />, document.querySelector("#overlay"))}
+      {!onStart && (
+        <div className="flex flex-col">
+          <Search setStartWith={setStartWith} />
+          {!onSearch && (
+            <h2 className="Search-text text-white mx-auto my-7 text-[3rem]">
+              Unlock worlds. One search away.
+            </h2>
+          )}
+        </div>
+      )}
+
       <div className="flex flex-wrap gap-4 justify-center p-4">
         {list.items.map((item) => (
           <Card key={item.id} element={item} />
